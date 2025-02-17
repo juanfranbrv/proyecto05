1342674a126e5c0374d86a9266cee26bb2faba18
juanfranbrv <juanfranbrv@gmail.com>
2025-02-17 14:52:49 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+
+import htmx from 'astro-htmx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [htmx()]
+});
